3c5c25f28efe72de8a683fe7b07732ccb60d84e1
Himanshu <himanshuparodiya@gmail.com>
2024-03-05 12:36:15 +0530

    ignore node_modules and dist folder change package.json (mandatory)

--- dist/components/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { ReactNode } from "react";
-type PropType = {
-    children: ReactNode;
-};
-declare const Button: ({ children }: PropType) => React.JSX.Element;
-export { Button };
